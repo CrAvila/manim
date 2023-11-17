de80826f968c82e1e4aa1bcaa3ea03145602e255
cravila <cravila@netphisolutions.com>
2023-11-17 01:22:08 +0000

    add: move to another obejct scene :tada:

--- 00-Test/C2/mm2.py
+++ 00-Test/C2/mm2.py
@@ -4,3 +4,19 @@ class MoveToScene(Scene):
     def construct(self):
         s = Circle()
         self.play(s.animate.to_edge(UP + RIGHT))
+        self.wait()
+
+        self.play(s.animate.shift(2*DOWN + 2*LEFT))
+
+class MoveToAnotherObject(Scene):
+    def construct(self):
+        h = Text("Hello")
+        s = Square()
+
+        self.add(h, s)
+
+        self.play(h.animate.to_edge(UP))
+        self.wait()
+        self.play(s.animate.next_to(h, DOWN))
+        
+        self.wait(2)
