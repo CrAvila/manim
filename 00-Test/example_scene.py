fc76218d0e5cd64a2bc295dba8918b16024f3ef6
cravila <cravila@netphisolutions.com>
2023-11-17 01:26:23 +0000

    add hq scenes

--- 00-Test/example_scene.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class SquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()                   # Create a circle
-        circle.set_fill(PINK, opacity=0.5)  # Set the color and transparency
-        self.play(Create(circle))           # Show the circle on screen
