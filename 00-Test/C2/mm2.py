71a9c57d03b6736b11dfe0d586485a44b14061db
cravila <cravila@netphisolutions.com>
2023-11-17 04:54:01 +0000

    add: color change scene :tada:

--- 00-Test/C2/mm2.py
+++ 00-Test/C2/mm2.py
@@ -16,7 +16,24 @@ class MoveToAnotherObject(Scene):
         self.add(h, s)
 
         self.play(h.animate.to_edge(UP))
-        self.wait()
         self.play(s.animate.next_to(h, DOWN))
+
+        sandt = VGroup(h, s)
+
+        self.play(sandt.animate.rotate(PI/4))
+
+        self.play(sandt.animate.shift(2*DOWN))
         
         self.wait(2)
+
+class ColorChange(Scene):
+    def construct(self):
+        t = Text("Some Text")
+        t.set_color(YELLOW_E)
+        self.play(Write(t), run_time=3)
+        
+        self.play(t.animate.scale(3))
+
+        self.play(t.animate.set_color_by_gradient(GREEN_E, PURPLE_E))
+
+        self.wait()
